e55740a55cf63fabacc334bbadfd956762e6ba8f
Md Limon Mia <mdlimonislam000000@gmail.com>
2026-09-19 23:34:23 +0600

    Problem 17 solved

--- index.js
+++ index.js
@@ -9,3 +9,25 @@ const countProperties = (obj) =>{
     return count ;
 }
 console.log('problem - 16 :',countProperties({a: 1, b: 2, c: 3 , d : 5}));
+
+// Problem - 17 
+
+const mergeObjects = (obj1, obj2) => {
+    const merged = {};
+
+    for (let key in obj1) {
+        if (Object.hasOwn(obj1, key)) {
+            merged[key] = obj1[key];
+        }
+    }
+    
+    for (let key in obj2) {
+        if (Object.hasOwn(obj2, key)) {
+            merged[key] = obj2[key];
+        }
+    }
+    
+    return merged;
+};
+
+console.log('Problem - 17 :',mergeObjects({a: 1, x: 10}, {b: 2, x: 20}));
